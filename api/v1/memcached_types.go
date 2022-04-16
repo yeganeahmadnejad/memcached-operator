06b9b50bf4fcac29f3006a7f779414b7ddd791d8
yeganeahmadnejad <yeganeahmadnejad@gmail.com>
2022-04-16 07:08:29 +0000

    change logic

--- api/v1/memcached_types.go
+++ api/v1/memcached_types.go
@@ -25,11 +25,12 @@ import (
 
 // MemcachedSpec defines the desired state of Memcached
 type MemcachedSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+    // INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+    // Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Memcached. Edit memcached_types.go to remove/update
-	Size int32 `json:"size"`}
+    // Foo is an example field of Memcached. Edit Memcached_types.go to remove/update
+    Size int32 `json:"size"`
+}
 
 // MemcachedStatus defines the observed state of Memcached
 type MemcachedStatus struct {
